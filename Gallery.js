42f14ff9aca9cb73cb7ae3ae8f23c0be0f00760e
GdelosReyes12 <gdelosreyes@addu.edu.ph>
2025-11-05 19:30:11 +0800

    Remove unnecessary newline in Gallery.js

---
+++ Gallery.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const uploadForm = document.getElementById('upload-form');
+    const successMessage = document.getElementById('upload-success-message');
+
+    if(uploadForm) {
+      uploadForm.addEventListener('submit', (event) => {
+        
+        event.preventDefault();
+
+        successMessage.style.display = 'block';
+
+        setTimeout(() => {
+          uploadForm.reset();
+        }, 3000);
+        
+        setTimeout(() => {
+          successMessage.style.display = 'none';
+        }, 5000);
+
+      });
+    }
+  });
