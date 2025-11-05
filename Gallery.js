d6dac0a7fc2bd89c7c92f9216309418da3e7ff7f
GdelosReyes12 <gdelosreyes@addu.edu.ph>
2025-11-05 19:30:45 +0800

    Update Gallery.js

--- Gallery.js
+++ Gallery.js
@@ -1,23 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const uploadForm = document.getElementById('upload-form');
     const successMessage = document.getElementById('upload-success-message');
-
     if(uploadForm) {
-      uploadForm.addEventListener('submit', (event) => {
-        
+      uploadForm.addEventListener('submit', (event) => {  
         event.preventDefault();
-
         successMessage.style.display = 'block';
-
+          
         setTimeout(() => {
           uploadForm.reset();
         }, 3000);
-        
         setTimeout(() => {
           successMessage.style.display = 'none';
         }, 5000);
-
       });
     }
   });
